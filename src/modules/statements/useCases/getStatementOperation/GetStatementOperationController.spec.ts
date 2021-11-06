66a901482ba623b7cc50840fa6d6868a46696644
ThallesP <contato@thalles.me>
2021-11-06 20:25:02 -0300

    :white_check_mark: Add more E2E tests

--- src/modules/statements/useCases/getStatementOperation/GetStatementOperationController.spec.ts
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationController.spec.ts
@@ -6,7 +6,7 @@ import request from "supertest";
 import { app } from "../../../../app";
 let connection: Connection;
 
-describe("Get balance controller", () => {
+describe("Get statement operation controller", () => {
   beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
@@ -27,7 +27,7 @@ describe("Get balance controller", () => {
     await connection.close();
   });
 
-  it("should be able to get user's balance", async () => {
+  it("should be able to get statement operation from user", async () => {
     const user = {
       name: "test",
       email: "test@test.com",
@@ -38,12 +38,22 @@ describe("Get balance controller", () => {
       .post("/api/v1/sessions")
       .send(user);
 
+    const responseStatement = await request(app)
+      .post("/api/v1/statements/deposit")
+      .set("Authorization", `Bearer ${responseToken.body.token}`)
+      .send({
+        amount: 700,
+        description: "Test deposit statement",
+      });
+
+    const { id } = responseStatement.body;
+
     const response = await request(app)
-      .get("/api/v1/statements/balance")
+      .get(`/api/v1/statements/${id}`)
       .set("Authorization", `Bearer ${responseToken.body.token}`);
 
     expect(response.status).toEqual(200);
-    expect(response.body.balance).toEqual(0);
-    expect(response.body.statement.length).toEqual(0);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.type).toEqual("deposit");
   });
 });
